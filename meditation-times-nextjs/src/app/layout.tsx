4a28d6c5d971880e860a44845ad03086b5bac508
geehyness <geehyness22@gmail.com>
2025-03-22 13:58:20 +0200

    firebase authentication

--- meditation-times-nextjs/src/app/layout.tsx
+++ meditation-times-nextjs/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Footer } from "@/components/Footer";
 import { Providers } from "@/components/Providers";
 import "./globals.css"; // Assuming you have a global styles file
+import { Header } from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "Meditation Times", // Replace with your actual title
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>
+          <Header />
           {children}
           <Footer />
         </Providers>
