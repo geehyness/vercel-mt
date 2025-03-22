bb0075f296fb5367f10ed3bf398e11219c9c3cf6
geehyness <geehyness22@gmail.com>
2025-03-22 09:10:35 +0200

    firebase authentication

--- meditation-times-nextjs/src/app/api/auth/bkup[...nextauth]/route.ts
+++
@@ -1,8 +0,0 @@
-import { authOptions } from "@/lib/auth"; // Import your authOptions
-import NextAuth from "next-auth";
-
-// Create the NextAuth handler
-const handler = NextAuth(authOptions);
-
-// Export the handler for both GET and POST requests
-export { handler as GET, handler as POST };
